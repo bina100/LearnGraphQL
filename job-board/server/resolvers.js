58103a088a9819c6ae70ab190166be56aaf4eb3a
binaro <binaro97@gmail.com>
2024-06-26 09:09:59 +0300

    schema

--- job-board/server/resolvers.js
+++ job-board/server/resolvers.js
@@ -1,11 +1,17 @@
+import { getCompany } from './db/companies.js'
+import { getJobs } from './db/jobs.js'
+
 export const resolvers = {
     Query: {
-       job: ()=>{
-        return {
-            id: 'test-id',
-            title: 'The Title',
-            description: 'The description.'
-        }
-       }
+        jobs: () => getJobs()
+    },
+    Job: {
+        company: (job)=>getCompany(job.companyId)
+        ,
+        date: (job) =>  toIsoDate(job.createdAt)
     }
+}
+
+function toIsoDate(value){
+    return value.slice(0, 'yyyy-mm-dd'.length)
 }
