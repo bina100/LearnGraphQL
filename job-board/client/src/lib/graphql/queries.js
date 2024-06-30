c0b94dd26d8dd6ac54f3374624b64c4236652b6d
bina100 <44175529+bina100@users.noreply.github.com>
2024-06-30 21:30:32 +0300

    setup

--- job-board/client/src/lib/graphql/queries.js
+++ job-board/client/src/lib/graphql/queries.js
@@ -1,4 +1,5 @@
-import { GraphQLClient, gql } from 'graphql-request'
+import {  ApolloClient,gql, InMemoryCache} from '@apollo/client'
+import { GraphQLClient } from 'graphql-request'
 import { getAccessToken } from '../auth'
 
 const client = new GraphQLClient('http://localhost:8080/graphql', {
@@ -9,6 +10,11 @@ const client = new GraphQLClient('http://localhost:8080/graphql', {
         }
         return {}
     }
+})
+
+const apolloClient = new ApolloClient({
+    uri: 'http://localhost:8080/graphql',
+    cache: new InMemoryCache()
 })
 
 export async function createJob({ title, description }) {
