ccd93e595622b391b4f9c90ef5098598be289e48
binaro <binaro97@gmail.com>
2024-07-01 12:39:20 +0300

    hooks

--- job-board/client/src/lib/graphql/hooks.js
+++ job-board/client/src/lib/graphql/hooks.js
@@ -1,5 +1,5 @@
-import { useQuery } from '@apollo/client';
-import { companyByIdQuery, jobByIdQuery, jobsQuery } from './queries';
+import { useQuery, useMutation } from '@apollo/client';
+import { companyByIdQuery, jobByIdQuery, jobsQuery, createJobMutation } from './queries';
 
 export function useCompany(id) {
   const { data, loading, error } = useQuery(companyByIdQuery, {
@@ -14,9 +14,33 @@ export function useJob(id) {
   });
   return { job: data?.job, loading, error: Boolean(error) };
 }
+
 export function useJobs() {
   const { data, loading, error } = useQuery(jobsQuery, {
     fetchPolicy:'network-only'
   });
   return { jobs: data?.jobs, loading, error: Boolean(error) };
+}
+
+export function useCreateJob(title, description) {
+    const [mutate, {loading}] = useMutation(createJobMutation)
+     const createJob = async (title, description)=>{
+        const {data: {job}} = await mutate({
+            variables: {input:{title, description}},
+            update: (cache, { data }) => {
+              cache.writeQuery({
+                query: jobByIdQuery,
+                variables: { id: data.job.id },
+                data,
+              });
+            }
+          })
+         return job 
+     }
+    
+    return {
+        createJob,
+        loading,
+
+    }
 }
--- job-board/client/src/pages/CreateJobPage.js
+++ job-board/client/src/pages/CreateJobPage.js
@@ -1,28 +1,18 @@
 import { useState } from 'react';
-import { createJob, createJobMutation, jobByIdQuery } from '../lib/graphql/queries';
 import { useNavigate } from 'react-router';
-import { useMutation } from '@apollo/client';
+import { useCreateJob } from '../lib/graphql/hooks';
 
 function CreateJobPage() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
-  const [mutate] = useMutation(createJobMutation)
+  const { createJob, loading } = useCreateJob();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const {data: {job}} = await mutate({
-      variables: {input:{title, description}},
-      update: (cache, { data }) => {
-        cache.writeQuery({
-          query: jobByIdQuery,
-          variables: { id: data.job.id },
-          data,
-        });
-      }
-    })
-    console.log('should post a new job:', job);
-    navigate(`/jobs/${job.id}`)
+    const job = await createJob(title, description);
+    console.log('job created:', job);
+    navigate(`/jobs/${job.id}`);
   };
 
   return (
@@ -54,7 +44,8 @@ function CreateJobPage() {
           </div>
           <div className="field">
             <div className="control">
-              <button className="button is-link" onClick={handleSubmit}>
+              <button className="button is-link" disabled={loading}
+                onClick={handleSubmit}>
                 Submit
               </button>
             </div>
