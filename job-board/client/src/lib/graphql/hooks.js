a808af2348ef61263f5d4c91b75432c7f60b57b1
binaro <binaro97@gmail.com>
2024-07-02 09:01:34 +0300

    pagination

--- job-board/client/src/lib/graphql/hooks.js
+++ job-board/client/src/lib/graphql/hooks.js
@@ -1,46 +1,47 @@
-import { useQuery, useMutation } from '@apollo/client';
-import { companyByIdQuery, jobByIdQuery, jobsQuery, createJobMutation } from './queries';
+import { useMutation, useQuery } from '@apollo/client';
+import { companyByIdQuery, createJobMutation, jobByIdQuery, jobsQuery } from './queries';
 
 export function useCompany(id) {
   const { data, loading, error } = useQuery(companyByIdQuery, {
-    variables: { id }
+    variables: { id },
   });
   return { company: data?.company, loading, error: Boolean(error) };
 }
 
 export function useJob(id) {
   const { data, loading, error } = useQuery(jobByIdQuery, {
-    variables: { id }
+    variables: { id },
   });
   return { job: data?.job, loading, error: Boolean(error) };
 }
 
-export function useJobs() {
+export function useJobs(limit, offset) {
   const { data, loading, error } = useQuery(jobsQuery, {
-    fetchPolicy:'network-only'
+    variables: { limit, offset },
+    fetchPolicy: 'network-only',
   });
   return { jobs: data?.jobs, loading, error: Boolean(error) };
 }
 
-export function useCreateJob(title, description) {
-    const [mutate, {loading}] = useMutation(createJobMutation)
-     const createJob = async (title, description)=>{
-        const {data: {job}} = await mutate({
-            variables: {input:{title, description}},
-            update: (cache, { data }) => {
-              cache.writeQuery({
-                query: jobByIdQuery,
-                variables: { id: data.job.id },
-                data,
-              });
-            }
-          })
-         return job 
-     }
-    
-    return {
-        createJob,
-        loading,
+export function useCreateJob() {
+  const [mutate, { loading }] = useMutation(createJobMutation);
 
-    }
+  const createJob = async (title, description) => {
+    const { data: { job } } = await mutate({
+      variables: { input: { title, description } },
+      update: (cache, { data }) => {
+        cache.writeQuery({
+          query: jobByIdQuery,
+          variables: { id: data.job.id },
+          data,
+        });
+      },
+    });
+    return job;
+  };
+
+  return {
+    createJob,
+    loading,
+  };
 }
